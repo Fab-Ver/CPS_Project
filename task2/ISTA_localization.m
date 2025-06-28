bca649838935e67bba546c2497af50761b763efc
Fabio Veroli <fabio.veroli@studio.unibo.it>
2025-06-29 00:23:58 +0200

    COMPLETE TASK 2

--- task2/ISTA_localization.m
+++ task2/ISTA_localization.m
@@ -1,11 +1,25 @@
 function [x_est, a_est] = ISTA_localization(y, G, lambda, nu, n, q)
+    % ISTA_localization estimates the position x and the attack vector a
+    % Inputs:
+    % - y: RSS measurement vector 
+    % - G: normalized dictionary + identity matrix
+    % - lambda: regularization term
+    % - nu: step size
+    % - n: number of location cells
+    % - q: number of sensors
+    % Output:
+    % - x_est: estimated location vector
+    % - a_est: estimated attack vector
+
     xa_est = zeros(n+q,1);
     max_iteration = 10000;
 
     for i=1:max_iteration
+        % Gradient step + soft thresholding
         xa_est = soft_thresholding(xa_est - nu * G' * (G * xa_est - y), nu * lambda);
     end 
-
+    
+    % Separate final estimate into position and attack
     x_est = xa_est(1:n);
     a_est = xa_est(n+1:end);
 end 
--- task2/task2.m
+++ task2/task2.m
@@ -7,10 +7,14 @@ load("localization_data.mat");
 lambda = 10; 
 n = 100;            %Number of cells
 q = 20;             %Number of sensors 
-G = normalize([D eye(q)]);
+
+G = normalize([D eye(q)]); % Concatenate and normalize columns
 nu = norm(G,2)^(-2);
 
 [x, a] = ISTA_localization(y, G, lambda, nu, n, q);
 
-[supp_x, ind_x] = max(x') % Consider only the state with the higher value
-supp_a = supp(a')
+[~, ind_x] = max(x'); % Consider only the state with the higher value
+supp_a = supp(a');    % Sensor indices detected as attacked
+
+fprintf('Estimated target position index: %d\n', ind_x);
+fprintf('Estimated attacked sensor indices: [%s]\n', num2str(supp_a));
